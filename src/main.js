c3f15b33caddd93a73c44880b493c948dac69bfb
kevin-prendergast <kevinprendergast@longboat.com>
2018-05-13 21:12:43 +0100

    simplify component

--- src/main.js
+++ src/main.js
@@ -4,6 +4,48 @@ import {seed} from './assets/seed';
 
 Vue.use(Vuex)
 
+const submissionComponent = {
+  template:
+  ` <div style="display: flex; width: 100%">
+      <figure class="media-left">
+        <img class="image is-64x64"
+          v-bind:src="submission.submissionImage">
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <p>
+            <strong>
+              <a v-bind:href="submission.url" class="has-text-info">
+                {{ submission.title }}
+              </a>
+              <span class="tag is-small">#{{ submission.id }}</span>
+            </strong>
+            <br>
+              {{ submission.description }}
+            <br>
+            <small class="is-size-7">
+              Submitted by:
+              <img class="image is-24x24"
+                v-bind:src="submission.avatar">
+            </small>
+          </p>
+        </div>
+      </div>
+      <div class="media-right">
+        <span class="icon is-small" v-on:click="upvote(submission.id)">
+          <i class="fa fa-chevron-up"></i>
+          <strong class="has-text-info">{{ submission.votes }}</strong>
+        </span>
+      </div>
+    </div>`,
+  props: ['submission', 'submissions'],
+  methods: {
+    upvote(submissionId){
+      store.commit('increment', submissionId)
+    }
+  }
+};
+
 const store = new Vuex.Store({
   state: {
     submissions: seed
@@ -25,9 +67,7 @@ new Vue({
       });
     }
   },
-  methods: {
-    upvote(submissionId){
-      store.commit('increment', submissionId)
-    }
+  components: {
+    'submission-component': submissionComponent
   }
 });
